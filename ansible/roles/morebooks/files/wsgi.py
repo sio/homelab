0805d0a2825794747a53a3f4f7c1a1e313fe1fbc
Vitaly Potyarkin <sio.wtf@gmail.com>
2018-06-21 16:13:50 +0300

    Replace hardcoded values with variables

--- ansible/roles/morebooks/files/wsgi.py
+++ ansible/roles/morebooks/files/wsgi.py
@@ -6,4 +6,4 @@
 import os.path
 from hlc.launcher import wsgi_app
 
-application = wsgi_app(os.path.join(os.path.dirname(__file__), "hlc_config.json"))
+application = wsgi_app(os.path.join(os.path.dirname(__file__), "hlc.json"))
