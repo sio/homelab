fb0151ab1938ecc7a442587501d2dd618b3e4afc
Vitaly Potyarkin <sio.wtf@gmail.com>
2020-01-31 16:52:18 +0300

    Add real tests

--- ansible/roles/interactive/molecule/tests/test_role.py
+++ ansible/roles/interactive/molecule/tests/test_role.py
@@ -9,3 +9,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_echo(host):
     cmd = host.run('echo hello')
     assert 'hello' in cmd.stdout
+
+
+def test_authorized_keys(host):
+    keys = host.file('/home/interactive/.ssh/authorized_keys')
+    assert keys.contains('ssh-ed25519')
+    assert keys.user == 'interactive'
+    assert keys.mode == 0o600
+    assert len(keys.content.splitlines()) == 1
+
+
+def test_symlinks(host):
+    vimrc = host.file('/home/interactive/.vimrc')
+    assert vimrc.is_symlink
+    assert vimrc.linked_to.endswith('dotfiles/vim/vimrc.link')
