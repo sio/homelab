36ebfbf74c6f4d1444311547f12b654f57bd31e5
Vitaly Potyarkin <sio.wtf@gmail.com>
2020-07-10 18:30:51 +0300

    Canary role for handling dumb UPS

---
+++ ansible/roles/canary/templates/canary.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Shutdown the computer if canary host is offline
+#
+# This script is useful for dealing with dumb UPS. Computers powered by UPS can
+# detect power outage by checking whether a 'canary' machine is online. Canary
+# machine is meant to be powered directly from mains in the same building.
+# Graceful shutdown can be attempted afterwards
+
+
+# Configuration options
+CANARY_HOST=${CANARY_HOST:-192.168.28.1}
+CANARY_TIMEOUT=${CANARY_TIMEOUT:-90}  # seconds
+
+
+# Internal variables
+CANARY_SHUTDOWN=${CANARY_SHUTDOWN:-shutdown -P now CANARY_SHUTDOWN}
+CANARY_CHECK=${CANARY_CHECK:-ping -c 1 -q $CANARY_HOST}
+CANARY_RETRY_DELAY=${CANARY_RETRY_DELAY:-1}  # seconds
+
+
+start_time=$(date +%s)
+while [[ $((start_time + CANARY_TIMEOUT)) > $(date +%s) ]]
+do
+    $CANARY_CHECK && exit 0
+    sleep "$CANARY_RETRY_DELAY"
+done
+$CANARY_SHUTDOWN
