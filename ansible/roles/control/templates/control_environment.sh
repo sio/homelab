8c34e7b8e61b3066c65cd6ab41785b6c35dff609
Vitaly Potyarkin <sio.wtf@gmail.com>
2020-10-14 11:58:58 +0000

    Use better name for role variable

--- ansible/roles/control/templates/control_environment.sh
+++ ansible/roles/control/templates/control_environment.sh
@@ -1,6 +1,6 @@
 # {{ ansible_managed }}
 
-export ANSIBLE_VAULT_KEYNAME={{ control_keyring_id_vault_password|quote }}
+export ANSIBLE_VAULT_KEYNAME={{ control_ansible_vault_keyname|quote }}
 export INFRA={{ control_infra_dir|quote }}
 export INVENTORY={{ control_inventory|quote }}
 export PLAYBOOK_ARGS=--vault-password-file="{{ control_scripts_dir }}/control-secrets-fetch.sh"
