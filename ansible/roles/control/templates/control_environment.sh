aebe04fe32aecd8ec1ae565450dd60955c0d28af
Vitaly Potyarkin <sio.wtf@gmail.com>
2020-10-14 11:37:56 +0000

    Fix syntax for --vault-password-file

--- ansible/roles/control/templates/control_environment.sh
+++ ansible/roles/control/templates/control_environment.sh
@@ -3,4 +3,5 @@
 export ANSIBLE_VAULT_KEYNAME={{ control_keyring_id_vault_password|quote }}
 export INFRA={{ control_infra_dir|quote }}
 export INVENTORY={{ control_inventory|quote }}
-export PLAYBOOK_ARGS='--vault-password-file "{{ control_scripts_dir }}/control-secrets-fetch.sh"'
+export PLAYBOOK_ARGS=--vault-password-file="{{ control_scripts_dir }}/control-secrets-fetch.sh"
+export SSH_AUTH_SOCK={{ control_sshagent_socket|quote }}
