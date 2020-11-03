2feb67e2fb8263e56934e7735cd96287647124f4
Vitaly Potyarkin <sio.wtf@gmail.com>
2020-11-03 11:06:24 +0300

    Use idempotent script to handle runner registration

---
+++ ansible/roles/gitlab_runner/templates/register-runners.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# {{ ansible_managed }}
+#
+# Register GitLab runners configured for this host
+#
+
+
+export CONFIG_FILE={{ gitlab_runner_config|quote }}
+
+gitlab-runner unregister --all-runners
+
+gitlab-runner register \
+    --non-interactive \
+    --url {{ gitlab_runner_ci_host|quote }} \
+    --registration-token {{ gitlab_runner_token|quote }} \
+    --executor {{ gitlab_runner_executor|quote }} \
+    {%- for key, value in gitlab_runner_extra_registration_params.items() %}
+    --{{ key }} {{ value|quote }} \
+    {% endfor -%}
+    --tag-list "{{ gitlab_runner_tags|join(",") }}"
