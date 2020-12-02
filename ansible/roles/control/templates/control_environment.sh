6508bfa96bdd58104b552a6c4e6b6f6f8ccf064e
Vitaly Potyarkin <sio.wtf@gmail.com>
2020-12-02 11:10:45 +0300

    Provide environment variables for CI/CD entrypoint

--- ansible/roles/control/templates/control_environment.sh
+++ ansible/roles/control/templates/control_environment.sh
@@ -4,4 +4,5 @@ export ANSIBLE_VAULT_KEYNAME={{ control_ansible_vault_keyname|quote }}
 export INFRA={{ control_infra_dir|quote }}
 export INVENTORY={{ control_inventory|quote }}
 export PLAYBOOK_ARGS=--vault-password-file="{{ control_scripts_dir }}/control-secrets-fetch.sh"
+export PLAYBOOK_RUNNER=~/bin/app.venv/ansible-playbook/bin/ansible-playbook
 export SSH_AUTH_SOCK={{ control_sshagent_socket|quote }}
