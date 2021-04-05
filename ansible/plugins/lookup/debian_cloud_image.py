9f54e7230dcd3884bb3d4cf2e8148b44dc4921bf
Vitaly Potyarkin <sio.wtf@gmail.com>
2021-04-05 10:13:38 +0300

    Finish image detection functionality

--- ansible/plugins/lookup/debian_cloud_image.py
+++ ansible/plugins/lookup/debian_cloud_image.py
@@ -1,5 +1,13 @@
+import platform
 import re
 import urllib.request
+
+
+def get_debian_cloud_image(release, image='generic', arch=None):
+    '''Return information about latest Debian Cloud image matching the provided parameters'''
+    directory = f'https://cdimage.debian.org/cdimage/cloud/{release}/daily/'
+    timestamp = get_latest_timestamp(directory)
+    return get_image_details(f'{directory}{timestamp}', image, timestamp)
 
 
 def fetch_html(url):
@@ -10,8 +18,9 @@ def fetch_html(url):
     return response.read().decode()
 
 
-def find_latest_date(html):
-    '''Find latest daily timestamp'''
+def get_latest_timestamp(url):
+    '''Find latest daily timestamp for Debian Cloud image on HTML page'''
+    html = fetch_html(url)
     date_regex = re.compile(r'\d{8}-\d+')
     timestamps = date_regex.findall(html)
     if not timestamps:
@@ -19,5 +28,38 @@ def find_latest_date(html):
     return sorted(timestamps)[-1]
 
 
+def get_image_details(url, image, timestamp, arch=None, filetype='qcow2'):
+    '''Get Debian cloud image URLs for the given timestamp'''
+    if not arch:
+        arch = platform.machine().lower()
+    html = fetch_html(url)
+
+    def single_match(regex):
+        '''Query regex for a single match against fetched HTML'''
+        matches = set(regex.findall(html))
+        if not matches:
+            raise ValueError(f'pattern ({regex.pattern}) was not found on page: {url}')
+        elif len(matches) != 1:
+            raise ValueError(f'pattern ({regex.pattern}) appears multiple times on page: {url}\n{matches}')
+        return matches.pop()
+
+    image_regex = re.compile(r'a href="(debian-\d+-{image}-{arch}-daily-{timestamp}\.{filetype})"'.format(
+        arch=arch,
+        filetype=filetype,
+        image=image,
+        timestamp=timestamp,
+    ))
+    image_name = single_match(image_regex)
+
+    checksum_regex = re.compile(r'a href="(SHA\d+SUMS)"')
+    checksum_name = single_match(checksum_regex)
+
+    return dict(
+        image=f'{url}/{image_name}',
+        checksum=f'{url}/{checksum_name}',
+        timestamp=timestamp,
+    )
+
+
 if __name__ == '__main__':
-    print(find_latest_date(fetch_html('http://cdimage.debian.org/cdimage/cloud/buster/daily/')))
+    print(get_debian_cloud_image('buster'))
