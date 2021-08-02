202dc1719c56b52e7567502c18bd9864dca74668
Vitaly Potyarkin <sio.wtf@gmail.com>
2021-08-02 08:49:05 +0000

    Do not assume that second pipeline is the previous one
    This commit adds logic to detect previous pipeline based on current
    pipeline id. Previously second newest pipeline was assumed to be the
    previous one which could lead to dead lock (pipeline #2 waiting on itself
    to finish)

--- .gitlab/ci/check_pipeline.py
+++ .gitlab/ci/check_pipeline.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-Show status of previous CI pipeline
+Translate status of previous CI pipeline into exit code
 
 Wait for the previous pipeline to finish if it's still running
 '''
@@ -9,37 +9,67 @@ Wait for the previous pipeline to finish if it's still running
 import json
 import os
 import urllib.request
+from datetime import datetime
 from time import sleep
 
 
+GITLAB_API = 'https://gitlab.com/api/v4'
+
+
 def check_environment():
-    required = ['CI_PROJECT_ID', 'CHECK_BRANCH', 'CHECK_PIPELINE']
+    required = ['CI_PROJECT_ID', 'CHECK_BRANCH', 'CI_PIPELINE_ID']
     missing = set(required).difference(os.environ)
     if missing:
         raise ValueError(f'Environment variable not defined: {" ".join(missing)}')
 
 
-def pipeline_status():
-    url = 'https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/pipelines?ref=$CHECK_BRANCH&per_page=1&page=$CHECK_PIPELINE'
-    url = os.path.expandvars(url)
+def fetch_json(url):
     response = urllib.request.urlopen(url)
     if response.status != 200:
         raise RuntimeError(f'HTTP {response.status}: {url}')
-    pipeline = json.load(response)[0]
-    return pipeline['status'], pipeline['web_url']
+    return json.load(response)
+
+
+def find_previous_pipeline(project_id, find_previous=1, branch='master', current_pipeline_id=None):
+    if current_pipeline_id is None:
+        current_pipeline_id = int(os.environ['CI_PIPELINE_ID'])
+    url = f'{GITLAB_API}/projects/{project_id}/pipelines?ref={branch}'
+    pipelines = fetch_json(url)
+
+    relative_idx = -1
+    for pipeline in sorted(pipelines,
+                           key=lambda p: datetime.fromisoformat(p['created_at'][:-1]),
+                           reverse=True):
+        if relative_idx >= 0:
+            relative_idx += 1
+        if pipeline['id'] == current_pipeline_id:
+            relative_idx = 0
+        if relative_idx == find_previous:
+            return pipeline
+    else:
+        raise RuntimeError(f'Current or previous pipeline not found among {len(pipelines)} newest pipelines')
+
+
+def pipeline_status(project_id, pipeline_id):
+    url = f'{GITLAB_API}/projects/{project_id}/pipelines/{pipeline_id}'
+    pipeline = fetch_json(url)
+    return pipeline['status']
 
 
 def main():
     check_environment()
+    project = int(os.environ['CI_PROJECT_ID'])
+    branch = os.environ['CHECK_BRANCH']
+    pipeline = find_previous_pipeline(project, branch=branch)
     while True:
-        status, url = pipeline_status()
+        status = pipeline_status(project, pipeline['id'])
         if status == 'running':
-            sleep(1)
+            sleep(2)
             continue
         elif status == 'success':
             break
         else:
-            raise RuntimeError(f'Pipeline {status}: {url}')
+            raise RuntimeError(f'Pipeline {status}: {pipeline["web_url"]}')
 
 
 if __name__ == '__main__':
