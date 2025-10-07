3aaf08533034b5c06819ba3cbc99e9ebaf94fcf3
Vitaly Potyarkin <sio.wtf@gmail.com>
2025-10-07 14:48:10 +0300

    fio: Configure reusable i/o benchmark

---
+++ fio/README.md
@@ -0,0 +1,5 @@
+# Disk i/o benchmarks with fio
+
+This directory contains reusable fio configuration for most common i/o
+performance tests. Use `make read`, `make write` and `make rw` to launch a
+specific subset of available tests or `make all` to execute all tests.
---
+++ fio/fio.mk
@@ -0,0 +1,34 @@
+FIO?=fio
+FILENAME?=/tmp/fio.bin
+SIZE=10G
+
+.DEFAULT_GOAL=read
+OPTIONS?=$(foreach section,$(SECTIONS),--section=$(section))
+CONFIG?=$(patsubst %.mk,%.config,$(lastword $(MAKEFILE_LIST)))
+LOG?=$(patsubst %.config,%.log,$(CONFIG))
+SECTIONS?=$(shell awk '$(section_names_awk)' $(CONFIG))
+
+define section_names_awk
+/^\s*\[[^\]]+\]/ {
+	gsub(/(\[|\])/, "");
+	if ($$0 == "global") next;
+	print $$0;
+}
+endef
+
+ifneq (,$(findstring /dev/,$(FILENAME)))
+OPTIONS+=--allow-file-create=0
+else
+OPTIONS+=--size=$(SIZE)
+endif
+OPTIONS+=--eta=always --eta-interval=2s --eta-newline=10s
+
+read: SECTIONS:=$(filter read-%,$(SECTIONS))
+write: SECTIONS:=$(filter write-%,$(SECTIONS))
+rw: SECTIONS:=$(filter rw-%,$(SECTIONS))
+all: SECTIONS=
+
+.PHONY: read write rw all
+read write rw all:
+	$(FIO) --filename=$(FILENAME) $(OPTIONS) $(CONFIG) | tee --append $(LOG)
+	@echo '\nBENCHMARK RESULTS'; grep -P "[\w-]+:.*groupid=.*jobs=" -A1 $(LOG)
